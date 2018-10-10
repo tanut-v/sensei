93d99123efa2f3268604f75be5404c42e5a31c20
Noppanan Sinroungrong <zen.noppanan@gmail.com>
2018-10-10 15:43:28 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :articles, only: %i[index new show edit]
+  namespace :api do
+    resource :articles, only: %i[create update destroy show]
+  end
 end
